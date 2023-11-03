36d22e95d60ae4ae8c0eabf8b5e7ab36144f7568
bryanjesuscastro2003 <jesusbryam624@gmail.com>
2023-11-03 15:22:21 -0600

    1520

---
+++ LogupService/src/main/java/com/app/domain/entity/RequestDomain.java
@@ -0,0 +1,10 @@
+package com.app.domain.entity;
+
+public class RequestDomain {
+    private String name;
+    private String lastname;
+    private String phoneNumber;
+    private String email;
+    private String username;
+    private String password;
+}
